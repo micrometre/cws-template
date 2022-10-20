fefc391aa683ed54bef9e220497dd65d2d044312
Henok Wehibe <henokmicrometre@gmail.com>
2022-10-20 11:53:23 +0100

    testing custom

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from "../components/Navbar";
+import Layout  from "../components/layout";
+import ThemeToggler from "../components/ThemeToggler";
+export default function Home() {
+  return (
+    <Layout>
+      <ThemeToggler/>
+    </Layout>
+  );
+}
